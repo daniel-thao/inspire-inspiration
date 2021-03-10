d6dbeecdc0eeeeadcc221dfdbb9cfde193675fcd
TabooThao <daniel.ntaj.thao@gmail.com>
2021-03-10 14:55:50 -0600

    heroku addons

--- client/src/contexts/AuthOProviderWithHistory.jsx
+++ client/src/contexts/AuthOProviderWithHistory.jsx
@@ -2,8 +2,8 @@ import { useHistory } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const Auth0ProviderWithHistory = ({ children }) => {
-  const domain = "dev-b9sse9-f.us.auth0.com";
-  const clientId = "KqL5SC94urdM2hAZGU4iAMgCMJHIvlrj";
+  const domain = process.env.AUTH0_DOMAIN || "dev-b9sse9-f.us.auth0.com";
+  const clientId = process.env.AUTH0_CLIENT_ID || "KqL5SC94urdM2hAZGU4iAMgCMJHIvlrj";
 
   const history = useHistory();
 
